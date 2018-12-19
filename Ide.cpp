08c7e583d2516414abee0f008fe2c4393f753edd
Nic Doorlay <n.doorlay@gmail.com>
2018-12-19 15:35:26 -0800

    added comments and shit

--- Ide.cpp
+++ Ide.cpp
@@ -2,27 +2,39 @@
 #include <cmath>
 
 
-int main() {
+int main() 
+{
+    //Asks for the value of a
     std::cout << "a : " << std::endl;
+    //Intitializes a
     double a;
     std::cin >> a;
     
+    //Asks for the value of b
     std::cout << "b : " << std::endl;
+    //Initializes b
     double b;
     std::cin >> b;
     
+    //Not sure what's going on here, I would know if you would use comments
     double poW = pow(a,b);
     
-    
-    if(poW > 100000){
+    //Conditial statment checking the value of poW
+    if(poW > 100000)
+    {
+        //Prints out that poW is big
         std::cout << poW << " is a big number" << std::endl;
     }
-    else{
+    else
+    {
+        //Pritns out that poW is not big
         std::cout << poW << " is not a big number" << std::endl;
     }
     
-    for(int i = 0; i < 3; i++){
-        std::cout << pow(a,b) << std::endl;
+    //Loops through to print the value of poW multiple times
+    for(int i = 0; i < 3; i++)
+    {
+        std::cout << poW(a,b) << std::endl;
     }
 	return 0;
 }
